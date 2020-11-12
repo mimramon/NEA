73ce6bfa3b9e1d93e40c7f5c364aba68677429e2
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-12 12:52:38 +0000

    start

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        Interaction();
+    }
+
+    //Movement handler
+    public void Movement()
+    {
+
+    }
+
+    //Interaction handler
+    Public void Interaction()
+    {
+
+    }
+}
