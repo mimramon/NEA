0d332f334bef61cbc54cddc87333affc8063fcab
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-16 11:23:17 +0000

    player manager started

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public CharacterController player; //Player's character controller is assigned in editor
+
     // Update is called once per frame
     void Update()
     {
@@ -16,11 +18,21 @@ public class PlayerManager : MonoBehaviour
     public void Movement()
     {
 
+        float x = Input.GetAxisRaw("Horizontal");//stores horizontal value
+        float z = Input.GetAxisRaw("Vertical");//stores vertical value
+        
+        Vector3 direction = new Vector3(x,0,z).normalized;//makes a vector based off the horizontal and vertical values, the .normalized makes the magnitude = 1
+        
+        if(direction.magnitude > 0.1f) //checks that the magnitude of the vector is bigger than 0.1
+        {
+            player.Move(direction);//moves the player in the vector direction
+        }
+        
     }
 
     //Interaction handler
-    Public void Interaction()
+    public void Interaction()
     {
-
+        
     }
 }
