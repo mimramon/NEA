e46097188e0d744edddffb6f6373d242b52f8d0a
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-19 12:14:25 +0000

    start cam script

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cam;
+
+    void Update() 
+    {
+        cam.Move(player.transform);
+    }
+
+}
