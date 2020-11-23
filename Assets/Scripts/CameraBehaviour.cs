f9b89af57188ec5b0b56601a31e9c91eae42bb1a
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-23 12:40:51 +0000

    camera rotation, offset not preserved

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,11 +6,17 @@ public class CameraBehaviour : MonoBehaviour
 {
     public Transform player; //player's  position
     public Transform cam; //camera's position
-    public Vector3 offset = new Vector3(0f, 0f, -10f); //Vector for the camera offset
-
+    
     void Update() 
     {
-        cam.position = player.position + offset;//every frame the cameras position is updated to the player position + the offset
+        Rotate(); 
     }
 
+    void Rotate()
+    {
+        if(Input.GetKey("q"))
+        {
+            cam.RotateAround(player.position, cam.up, 20 * Time.deltaTime);//rotates camera around player
+        }
+    }
 }
