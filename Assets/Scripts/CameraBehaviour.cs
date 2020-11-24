35b828b8b4f0000e4a27f6abc7d92e524e613fd4
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-24 15:31:09 +0000

    camera rotation fixed

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,17 +6,21 @@ public class CameraBehaviour : MonoBehaviour
 {
     public Transform player; //player's  position
     public Transform cam; //camera's position
+    public Vector3 offset = new Vector3(0,0,-10); //
+    public float currentYaw = 0; //
+    public float yawSpeed = 100; //
+    public float pitch =2; //
     
     void Update() 
     {
-        Rotate(); 
+        currentYaw -= Input.GetAxis("CamHorizontal") * yawSpeed * Time.deltaTime;
     }
 
-    void Rotate()
+    void LateUpdate() 
     {
-        if(Input.GetKey("q"))
-        {
-            cam.RotateAround(player.position, cam.up, 20 * Time.deltaTime);//rotates camera around player
-        }
+        transform.position = player.position - offset;
+        transform.LookAt(player.position + Vector3.up * pitch);
+
+        cam.RotateAround(player.position, Vector3.up, currentYaw);//rotates camera around player
     }
 }
