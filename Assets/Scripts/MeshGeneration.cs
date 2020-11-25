deb9214fa7a4b6c41bcffae5d8e5efa952d12783
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-25 15:22:09 +0000

    started mesh generation script

---
+++ Assets/Scripts/MeshGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGeneration : MonoBehaviour
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+    }
+
+    // Update is called once per frame
+    public void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2
+        };
+    }
+}
