c5d6b4ac59da22c3271324cf8b3e35d43106b73d
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-11-26 12:42:34 +0000

    mesh is generated but no collider is added

--- Assets/Scripts/MeshGeneration.cs
+++ Assets/Scripts/MeshGeneration.cs
@@ -7,6 +7,8 @@ public class MeshGeneration : MonoBehaviour
 {
     public Vector3[] vertices;
     public int[] triangles;
+    public int gridX = 50;
+    public int gridZ = 50;
     Mesh mesh;
 
     // Start is called before the first frame update
@@ -21,17 +23,41 @@ public class MeshGeneration : MonoBehaviour
     // Update is called once per frame
     public void CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0)
-        };
+        vertices = new Vector3[(gridX + 1) * (gridZ + 1)];
 
-        triangles = new int[]
+        int i = 0;
+        
+        for(int z = 0;z <= gridZ;z++)
         {
-            0,1,2
-        };
+            for(int x = 0;x <= gridX;x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2;
+                vertices[i] = new Vector3(x,y,z);
+                i++;
+            }
+        }
+
+        triangles = new int[gridX * gridZ * 6];
+        int tris = 0;
+        int vert = 0;
+
+        for(int z = 0;z < gridZ;z++)
+        {
+            for(int x = 0;x < gridX;x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + gridX + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + gridX + 1;
+                triangles[tris + 5] = vert + gridX + 2;
+
+                vert++;
+                tris +=6 ;
+
+            }
+            vert++;
+        }
     }
 
     public void UpdateMesh()
@@ -39,5 +65,6 @@ public class MeshGeneration : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
