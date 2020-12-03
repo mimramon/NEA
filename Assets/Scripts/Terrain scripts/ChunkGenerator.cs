49ceab5629b29c74b335b34ad6f6a94b28f276b0
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-12-03 09:22:35 +0000

    minute 6 of ep 4

--- Assets/Scripts/Terrain scripts/ChunkGenerator.cs
+++ Assets/Scripts/Terrain scripts/ChunkGenerator.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ChunkGenerator : MonoBehaviour
 {
+    public enum DrawMode
+    {
+        NoiseMap, colourMap
+    }
+    public DrawMode drawMode;
+
     public int mapWidth; //chunk width
     public int mapHeight; //chunk height
     public float noiseScale; //scale of noise
@@ -14,13 +20,39 @@ public class ChunkGenerator : MonoBehaviour
     public int seed;
     public Vector2 offset;
     public bool autoUpdate; //editor variable
+    public TerrainType[] regions;
 
     public void GenerateChunk() //generates the chunk
     {
         float[,] noiseMap = NoiseGenerator.NoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity, seed, offset); //gets the noisemap
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>(); //gets MapDisplay object
-        display.DrawNoiseMap(noiseMap); //draws the noisemap
+        if(drawMode == DrawMode.NoiseMap)
+        {
+           display.DrawNoiseMap(noiseMap); //draws the noisemap 
+        }
+        else if(drawMode == DrawMode.colourMap)
+        {
+
+        }
+        
     }
 
     void OnValidate()
@@ -41,5 +73,17 @@ public class ChunkGenerator : MonoBehaviour
         {
             octaves = 1;
         }
+        if(noiseScale < 10)
+        {
+            noiseScale = 10;
+        }
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public float height;
+    public Color colour;
+    public string name;
 }
---
+++ Assets/Scripts/Terrain scripts/TextureGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator
+{
+    public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colourMap);
+        texture.Apply();
+        return texture;
+    }
+
+    public static Texture2D TextureFromHeightMap(float[,] heightMap)
+    {
+        
+    }
+}
