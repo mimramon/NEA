3bfa92a912bec4752fdf75de9281e312803b3598
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2020-12-14 12:34:56 +0000

    started objects for data storage

---
+++ Assets/Scripts/Terrain scripts/Data structures/NoiseData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NoiseData", menuName = "Data Objects/Noise Data", order = 0)]
+public class NoiseData : ScriptableObject 
+{
+    
+}
+
+
---
+++ Assets/Scripts/Terrain scripts/Data structures/TerrainData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TerrainData", menuName = "Data Objects/Terrain Data", order = 0)]
+public class TerrainData : ScriptableObject 
+{
+    
+}
